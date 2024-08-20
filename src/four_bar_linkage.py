1772acd769c589e3c03446fa685f28c66d123fc3
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2024-08-20 10:00:19 +0300

    Create four_bar_linkage.py

---
+++ src/four_bar_linkage.py
@@ -0,0 +1,25 @@
+class FourBarLinkage:
+    # all parameters you need to calculate coordinates, default initializing
+    # for example 
+    # AB = 0
+    # ...
+    # alpha = ...
+    # ...
+    # all coordinates of conection points A,B,C,D,P
+    # A = (0.,0.)
+    # ...
+    # P = (0.,0.)
+    def __init__(self): #(self, AB, ..., alpha, ...):
+        # initialize all parameters
+        # for example
+        # self.AB = AB
+        # ...
+        # self.alpha = alpha
+        # initialize coordinates of connection points
+        self.run()
+    
+    def run(self):
+        # calculate and set all parameters
+        # self.A = ...
+        # ...
+        pass # delete later   
