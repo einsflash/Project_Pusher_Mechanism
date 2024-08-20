8b9d25569a71f226a8676941b7e87df8e0b40d04
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2024-08-20 10:29:48 +0300

    Update four_bar_linkage.py

--- src/four_bar_linkage.py
+++ src/four_bar_linkage.py
@@ -9,6 +9,9 @@ class FourBarLinkage:
     # A = (0.,0.)
     # ...
     # P = (0.,0.)
+    
+    # this is constructor that will be used from GUI to initialize all needed parameters,
+    # lengths, angles, coordinated of fixed points and so one
     def __init__(self): #(self, AB, ..., alpha, ...):
         # initialize all parameters
         # for example
@@ -18,6 +21,8 @@ class FourBarLinkage:
         # initialize coordinates of connection points
         self.run()
     
+    # implement here geometry to calculate coordinates of all points from parameters (lengths, angles, ...)
+    # this function will be called from GUI after updating some parameters to get coordinates for GUI animation
     def run(self):
         # calculate and set all parameters
         # self.A = ...
